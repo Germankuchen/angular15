05a2dda39ea252eb8b81fd624a60932459fe0eb6
GermanKuchen <germankuchen@gmail.com>
2019-08-12 13:07:42 -0300

    nuevo

--- src/app/components/carga/carga.component.ts
+++ src/app/components/carga/carga.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-carga',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CargaComponent implements OnInit {
 
+  archivos: Item[];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  cargarImagen() {
+
   }
 
 }
