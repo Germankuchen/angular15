23ba9e2967529a9c09dafdc44ded8dcf3be90da9
GermanKuchen <germankuchen@gmail.com>
2019-08-12 12:41:18 -0300

    Lo ultimo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { APP_ROUTING } from './app.routes';
+import { CargaImagenService } from './services/carga-imagen.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotosComponent,
+    CargaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [CargaImagenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
